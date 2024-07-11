a0c7088a32e96f700971808dcf769bed0935fb8c
Vedang <vedangssjgod@gmail.com>
2024-07-11 08:56:32 +0530

    Added login page and signup Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './page/Home'
+import Login from './page/login/Login'
+import SignUp from './page/signup/SignUp'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-          <div className='labels'>
-            <div className='labelItem'>All</div>
-            <div className='labelItem'>Pork</div>
-            <div className='labelItem'>Seafood</div>
-            <div className='labelItem'>Chicken</div>
-            <div className='labelItem'>Beef</div>
-            <div className='labelItem'>Vegetarian</div>
-            <div className='labelItem'>Celebrations on Board</div>
-          </div>
-        <div className='items'>
-            <div>Item 1</div>
-            <div>Item 2</div>
-            <div>Item 3</div>
-        </div>
-      </div>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/signup' element={<SignUp />}></Route>
+    </Routes>
+      
       <div>
 
       </div>
---
+++ frontend/src/page/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+          <div className='labels'>
+            <div className='labelItem'>All</div>
+            <div className='labelItem'>Pork</div>
+            <div className='labelItem'>Seafood</div>
+            <div className='labelItem'>Chicken</div>
+            <div className='labelItem'>Beef</div>
+            <div className='labelItem'>Vegetarian</div>
+            <div className='labelItem'>Celebrations on Board</div>
+          </div>
+        <div className='items'>
+            <div>Item 1</div>
+            <div>Item 2</div>
+            <div>Item 3</div>
+        </div>
+      </div>
+  )
+}
+
+export default Home
