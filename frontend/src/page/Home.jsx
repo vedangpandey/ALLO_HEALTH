391cdf8a6fbffc4d29557e6783369017d6cb5968
Vedang <vedangssjgod@gmail.com>
2024-07-11 23:26:44 +0530

    Revert "Completed Project"
    This reverts commit e5376009aded083c2d6c17164ba720d6e071a3f7.

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,19 +1,11 @@
 import React, { useState } from 'react'
 import MenuItems from '../components/MenuItems'
-import Selection from '../components/Selection'
-import Meals from '../components/Meals'
 
 const Home = () => {
   const [data,setData] = useState(null)
   return (
-    <div className='flex w-full h-full overflow-x-hidden'>
+    <div className='flex w-screen h-screen bg-gray-300 overflow-x-hidden'>
          <MenuItems data={data} setData={setData} className="flex-shrink-0 max-w-[300px] overflow-y-auto" />
-         <div className="flex flex-col">
-          <Selection className="mb-4" />
-          <div className="flex flex-col">
-            <Meals className="p-4" />
-          </div>
-        </div>
       </div>
   )
 }
